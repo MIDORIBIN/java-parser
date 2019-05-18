bbdf5364156a5a190aee0ba50a3aaef043e49e51
近藤郁哉 <>
2019-05-18 17:16:09 +0900

    add: コードをbase64で渡すように修正

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import json
+import base64
 
 from ClassInfo import ClassInfo
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-app.config.update({'DEBUG': True})
 
 
 @app.route('/', defaults={'path': ''})
@@ -41,12 +41,20 @@ def sample():
 def analyze():
     data = request.data.decode('utf-8')
     data = json.loads(data)
-    code = str(data['key'])
 
-    class_info = ClassInfo(code)
-    body_json = json.loads(json.dumps(class_info, default=(lambda o: o.__dict__)))
+    class_info_list = list(map(base64_to_class_info, data['code_list']))
+    body_json = json.loads(json.dumps(class_info_list, default=(lambda o: o.__dict__)))
 
     return jsonify(body_json)
+
+
+def base64_to_code(base64_code: str) -> str:
+    return base64.b64decode(base64_code).decode('utf-8')
+
+
+def base64_to_class_info(base64_code: str) -> ClassInfo:
+    code = base64_to_code(base64_code)
+    return ClassInfo(code)
 
 
 if __name__ == '__main__':
