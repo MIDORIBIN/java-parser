7fce2b92b9dc6e55d14543260d4c5e7f20a97ae2
近藤郁哉 <>
2019-05-28 23:12:49 +0900

    add: クラスのタイプを追加

--- class_info.py
+++ class_info.py
@@ -9,6 +9,7 @@ from parse_regex import CLASS_NAME_LINE, ACCESS_MODIFIER_LINE
 class ClassInfo:
     def __init__(self, code: str):
         self.class_name = ClassInfo.extra_class_name(code)
+        self.class_type = ClassInfo.extra_class_type(code)
         self.variable_info_list = VariableInfo.code_to_list(code)
         self.method_info_list = MethodInfo.code_to_list(code)
 
@@ -28,17 +29,44 @@ class ClassInfo:
         class_name_line = list(filter(pattern.match, code_list))[0]
         return pattern.match(class_name_line).group(2)
 
+    @staticmethod
+    def extra_class_type(code: str) -> str:
+        code_list = code.split('\n')
+        pattern = re.compile(CLASS_NAME_LINE)
+        class_name_line = list(filter(pattern.match, code_list))[0]
+        return pattern.match(class_name_line).group(1)
+
 
 def main():
-    code = '''
+    interface_sample = '''
 public interface SampleInterface {
     public String normal();
     public void arg(String name);
     public void args(String name, int age);
 }
     '''
-    a = ClassInfo(code)
-    print(json.dumps(a, default=(lambda o: o.__dict__), indent=4))
+    class_sample = """
+public class TestClass {
+    private String name;
+    private String id = "idid";
+    public static int age;
+
+    public TestClass(String name) {
+        this.name = name;
+    }
+    public String normalMethod(){
+        return "Engineer";
+    }
+    public static MyClass classMethod(MyClass myClass) {
+        return myClass;
+    }
+    private void multiArgMethod(String id, String age) {
+    }
+}"""
+
+    print(json.dumps(ClassInfo(interface_sample), default=(lambda o: o.__dict__), indent=4))
+    print('==========================================================================================')
+    print(json.dumps(ClassInfo(class_sample), default=(lambda o: o.__dict__), indent=4))
 
 
 if __name__ == '__main__':
--- parse_regex.py
+++ parse_regex.py
@@ -7,7 +7,14 @@ ONE_BLANK = r' +'
 ANYTHING = '(?: +(.*))?'
 PARENTHESES = '\((.*)\)'
 
+# public +(class|interface) +(\w+)
 CLASS_NAME_LINE = r'public' + ONE_BLANK + CLASS_NAME + ONE_BLANK + WARD
+
+# *public | private | protected
 ACCESS_MODIFIER_LINE = ZERO_BLANK + ACCESS_MODIFIER
-VARIABLE_LINE = ZERO_BLANK + '(' + ACCESS_MODIFIER + ')' + STATIC + ONE_BLANK + WARD + ONE_BLANK + WARD + '(.*);'
+
+# *(public | private | protected)(+static)? +(\w+) + (\w+)(.*);
+VARIABLE_LINE = ZERO_BLANK + '(' + ACCESS_MODIFIER + ')' + STATIC + ONE_BLANK + WARD + ONE_BLANK + WARD + '([^\(]*);'
+
+# *(public | private | protected)(+static)?(?: +(.*))? +(\w+) *\((.*)\)
 METHOD_LINE = ZERO_BLANK + '(' + ACCESS_MODIFIER + ')' + STATIC + ANYTHING + ONE_BLANK + WARD + ZERO_BLANK + PARENTHESES
